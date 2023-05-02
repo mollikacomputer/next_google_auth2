5988a727963a8b05bf6c61bf35170fb2bb810b07
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-03 01:36:48 +0600

    chat bot install

---
+++ pages/Config.js
@@ -0,0 +1,8 @@
+import { createChatBotMessage } from "react-chatbot-kit";
+
+const config = {
+  initialMessages: [createChatBotMessage(`Hello world`)],
+  botName:"MollikaComputerBox"
+}
+
+export default config
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
+import { Inter } from 'next/font/google';
+import {Chatbot} from 'react-chatbot-kit';
+import Config from './Config';
+import ActionProvider from './ActionProvider';
+import MessageParser from './MessageParser';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -9,6 +12,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <h2 className="text-2xl">Home Page </h2>
+      <Chatbot config={Config} actionProvider={ActionProvider} messageParser={MessageParser} />
     </main>
   )
 }
