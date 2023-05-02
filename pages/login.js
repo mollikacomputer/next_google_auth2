4da7cdb31926fcecfbe3bb7f0a24944716748518
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-02 23:32:46 +0600

    first commit

---
+++ pages/login.js
@@ -0,0 +1,22 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const login = () => {
+    const {data:session} = useSession();
+    if(session){
+        return(
+            <div>
+                <p> Welcome, {session.user.name},  {session.user.email} </p>
+                <button onClick={()=> signOut() } className="btn btn-success">SignOut</button>
+            </div>
+        )
+    }else{
+        return(
+            <div>
+                <p> You are not signIn</p>
+                <button onClick={()=> signIn('google') } className="btn btn-success">SignIn</button>
+            </div>
+        )
+    }
+};
+
+export default login;
