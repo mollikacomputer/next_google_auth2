8902a887e8bf35c9f0351c2be90487a228ae6a98
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-03 20:10:02 +0600

    some link added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <h2 className="text-2xl"> Home Page </h2>
+      <Link href={'/login'} > Login Page</Link>
       
     </main>
   )
--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 const login = () => {
     const {data:session} = useSession();
@@ -7,6 +8,7 @@ const login = () => {
             <div>
                 <p> Welcome, {session.user.name},  {session.user.email} </p>
                 <button onClick={()=> signOut() } className="btn btn-success">SignOut</button>
+                <Link href={'/'} > Back to Home Page</Link>
             </div>
         )
     }else{
@@ -15,7 +17,7 @@ const login = () => {
                 <p> You are not signIn</p>
                 <button onClick={()=> signIn('google') } className="btn btn-success mr-4"> SignIn with google </button>
                 <button onClick={()=> signIn('github') } className="btn btn-success mr-4"> SignIn with GitHub </button>
-                
+                <Link href={'/'} > Back to Home Page</Link>
             </div>
         )
     }
