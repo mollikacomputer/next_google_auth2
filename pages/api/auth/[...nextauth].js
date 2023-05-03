10a03717e27f0f5b568a3dcdbe3a7a80af7ae8bb
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-03 19:48:10 +0600

    some file deleted

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ export default NextAuth({
             clientId:process.env.GOOGLE_CLIENT_ID,
             clientSecret:process.env.GOOGLE_CLIENT_SECRET,
         }),
+        
     ],
     secret:"a94a3s+451atc"
 })
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 
 import { Inter } from 'next/font/google';
-import {Chatbot} from 'react-chatbot-kit';
-import Config from './Config';
-import ActionProvider from './ActionProvider';
-import MessageParser from './MessageParser';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -11,8 +8,8 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <h2 className="text-2xl">Home Page </h2>
-      <Chatbot config={Config} actionProvider={ActionProvider} messageParser={MessageParser} />
+      <h2 className="text-2xl"> Home Page </h2>
+      
     </main>
   )
 }
