ace9e94c4a2a9b67c6adf8d79261f1cd2a2e2ff0
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-03 20:15:06 +0600

    some comma delete

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,11 +6,11 @@ export default NextAuth({
     providers:[
         GoogleProvider({
             clientId:process.env.GOOGLE_CLIENT_ID,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET
         }),
         GitHubProvider({
             clientId:process.env.GITHUB_CLIENT_ID,
-            clientSecret:process.env.GITHUB_CLIENT_SECRET,
+            clientSecret:process.env.GITHUB_CLIENT_SECRET
         }),
 
     ],
